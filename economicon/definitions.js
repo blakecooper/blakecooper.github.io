c2e49aff9f9d1c4f80c416be4d472a9cd49ea5e7
Blake <contact@blake-cooper.com>
2019-10-24 13:35:36 -0400

    added html to popovers

--- economicon/definitions.js
+++ economicon/definitions.js
@@ -1,6 +1,6 @@
 const definitions = [
 		[
-			"centralReserveBank", 
+			"reserveBank", 
 			"<p>A nation's central reserve bank is where all gold, commodities and currencies belonging to the nation are kept.<p>The amount of gold held by bankers in their reserves determines the play order. The banker whose nation has the most gold gets to go first whenever the order of play matters.<p>Bankers use gold and native currency in their reserves to make purchases in the market. When they do, they determine the relative value of their purchase and place that amount of gold or native currency in the market from their reserves.<p>At the end of the game, bankers count up the value of all gold, commodities and currency in their reserve, relative to gold (that is, the amount of gold in the market is the numerator in the value formula). If a banker has the most valuable reserves, without having three or greater volatility tokens, that banker wins the game."
 		],
 ];
